a8ca66d55a9b78e489dff9b384076bffe05aabe8
Oprea Robert-Andrei <robertoprea716@gmail.com>
2026-03-23 03:06:12 +0200

    Initial source code and tests upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file main.cpp
+ * @brief Entry point for the Password Manager Application.
+ * * @details This application provides a secure environment for storing credentials
+ * using SQLite for data persistence, SHA-256 for master access security,
+ * and FTXUI for a modern Terminal User Interface (TUI).
+ * * -------------------------------------------------------------------------
+ * @project   PASSWORD MANAGER
+ * @author    OPREA ROBERT-ANDREI
+ * @year      2026
+ * -------------------------------------------------------------------------
+ */
+
+#include "interface.h"
+
+int main() {
+    ui();
+
+    return 0;
+}
